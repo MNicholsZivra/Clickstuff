adafd7a4bae8a1cfbc3b30461ecbaaa8eace2f65
Matt <Mnichols@zivra.com>
2017-08-08 15:10:59 +0000

    script changes

--- clickscript.js
+++ clickscript.js
@@ -9,15 +9,15 @@ function clickFunction(){
 	document.getElementById("displaypnt").innerHTML = clicks;
 	
 	if (clicks >= 50){
-		document.getElementById("bs").innerHTML = "50 Clicks, nice work queer!";
+		document.getElementById("bs").innerHTML = "50 Clicks, nice!";
 	}
 	
 	if (clicks >= 75){
-		document.getElementById("bs").innerHTML = "75, nice!";
+		document.getElementById("bs").innerHTML = "75, great clicking!!";
 	}
 	
 	if (clicks >= 100){
-		document.getElementById("bs").innerHTML = "100 Clicks, take a break tim!";
+		document.getElementById("bs").innerHTML = "100 Clicks, take a break!";
 	}
 	
 	if (clicks >= 110){
